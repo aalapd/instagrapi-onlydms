404c705b623b2faa1eeb390ba0c6265477d03b90
Aalap <30842327+aalapd@users.noreply.github.com>
2025-02-06 03:33:25 +0530

    Remove unused mixins and related methods from the instagrapi package

--- instagrapi/__init__.py
+++ instagrapi/__init__.py
@@ -2,39 +2,22 @@ import logging
 from urllib.parse import urlparse
 
 import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from urllib3.exceptions import InsecureRequestWarning
 
 from instagrapi.mixins.account import AccountMixin
-from instagrapi.mixins.album import DownloadAlbumMixin, UploadAlbumMixin
 from instagrapi.mixins.auth import LoginMixin
 from instagrapi.mixins.bloks import BloksMixin
 from instagrapi.mixins.challenge import ChallengeResolveMixin
-from instagrapi.mixins.clip import DownloadClipMixin, UploadClipMixin
-from instagrapi.mixins.collection import CollectionMixin
-from instagrapi.mixins.comment import CommentMixin
 from instagrapi.mixins.direct import DirectMixin
-from instagrapi.mixins.fbsearch import FbSearchMixin
-from instagrapi.mixins.hashtag import HashtagMixin
-from instagrapi.mixins.highlight import HighlightMixin
-from instagrapi.mixins.igtv import DownloadIGTVMixin, UploadIGTVMixin
-from instagrapi.mixins.insights import InsightsMixin
-from instagrapi.mixins.location import LocationMixin
-from instagrapi.mixins.media import MediaMixin
-from instagrapi.mixins.notification import NotificationMixin
 from instagrapi.mixins.password import PasswordMixin
-from instagrapi.mixins.photo import DownloadPhotoMixin, UploadPhotoMixin
 from instagrapi.mixins.private import PrivateRequestMixin
 from instagrapi.mixins.public import (
     ProfilePublicMixin,
     PublicRequestMixin,
     TopSearchesPublicMixin,
 )
-from instagrapi.mixins.share import ShareMixin
-from instagrapi.mixins.story import StoryMixin
-from instagrapi.mixins.timeline import ReelsMixin
 from instagrapi.mixins.totp import TOTPMixin
 from instagrapi.mixins.user import UserMixin
-from instagrapi.mixins.video import DownloadVideoMixin, UploadVideoMixin
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -46,32 +29,10 @@ class Client(
     TopSearchesPublicMixin,
     ProfilePublicMixin,
     LoginMixin,
-    ShareMixin,
-    FbSearchMixin,
-    HighlightMixin,
-    DownloadPhotoMixin,
-    UploadPhotoMixin,
-    DownloadVideoMixin,
-    UploadVideoMixin,
-    DownloadAlbumMixin,
-    NotificationMixin,
-    UploadAlbumMixin,
-    DownloadIGTVMixin,
-    UploadIGTVMixin,
-    MediaMixin,
     UserMixin,
-    InsightsMixin,
-    CollectionMixin,
     AccountMixin,
     DirectMixin,
-    LocationMixin,
-    HashtagMixin,
-    CommentMixin,
-    StoryMixin,
     PasswordMixin,
-    DownloadClipMixin,
-    UploadClipMixin,
-    ReelsMixin,
     BloksMixin,
     TOTPMixin,
 ):
